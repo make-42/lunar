fc82ad70103e858de67de25130c1ad451f056a6b
Louis Dalibard <louis.dalibard@gmail.com>
2026-02-10 22:03:24 +0100

    feat: add server

--- go.mod
+++ go.mod
@@ -4,12 +4,28 @@ go 1.25.5
 
 require (
 	github.com/fogleman/gg v1.3.0
+	github.com/gofiber/fiber/v3 v3.0.0
 	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
 	github.com/sixdouglas/suncalc v0.0.0-20250114185126-291b1938b70c
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.3 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/tinylib/msgp v1.6.3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.69.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/image v0.35.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image/png"
+	"log"
 	"math"
 	"time"
 
 	"github.com/fogleman/gg"
+	"github.com/gofiber/fiber/v3"
 	"github.com/sixdouglas/suncalc"
 
 	"lunar/config"
@@ -26,11 +29,64 @@ func SignedAngleDiff(a, b float64) float64 {
 	return diff - math.Pi
 }
 
+func genImage(mp suncalc.MoonPosition, mi suncalc.MoonIllumination) *gg.Context {
+	visAngle := SignedAngleDiff(mi.Angle, mp.ParallacticAngle)
+	//fmt.Println(mp.ParallacticAngle, mi.Angle)
+	anglePhase := mi.Phase * (2 * math.Pi)
+	//fmt.Println(visAngle*360/(2*math.Pi), mi.Phase)
+	dc := gg.NewContext(config.Config.ImageSize, config.Config.ImageSize)
+	dc.SetLineWidth(config.Config.LineWidth)
+	dc.DrawCircle(float64(config.Config.ImageSize)/2, float64(config.Config.ImageSize)/2, config.Config.MoonRad)
+	dc.SetColor(config.OutColor)
+	dc.Stroke()
+	currX, currY := screenCoords(1, 0, visAngle)
+	dc.MoveTo(currX, currY)
+	for i := 0; i < config.Config.LineRes; i++ {
+		phi := math.Pi * float64(i+1) / float64(config.Config.LineRes)
+		if math.Sin(anglePhase) > 0 {
+			currX, currY = screenCoords(math.Cos(phi), math.Sin(phi)*math.Cos(anglePhase), visAngle) // Should be opposite if sin < 0 for anglePhase
+		} else {
+			currX, currY = screenCoords(math.Cos(phi), -math.Sin(phi)*math.Cos(anglePhase), visAngle) // Should be opposite if sin < 0 for anglePhase
+		}
+		dc.LineTo(currX, currY)
+	}
+	for i := 0; i < config.Config.LineRes; i++ {
+		phi := math.Pi * (1 - float64(i+1)/float64(config.Config.LineRes))
+		if math.Sin(anglePhase) > 0 {
+			currX, currY = screenCoords(math.Cos(phi), math.Sin(phi), visAngle) // Should be opposite if sin < 0 for anglePhase
+		} else {
+			currX, currY = screenCoords(math.Cos(phi), -math.Sin(phi), visAngle) // Should be opposite if sin < 0 for anglePhase
+		}
+		dc.LineTo(currX, currY)
+	}
+	dc.Fill()
+
+	dc.SetLineWidth(config.Config.HorizonLineWidth)
+	dc.SetDash(5, 10)
+	frac := mp.Altitude / (moonRadius / mp.Distance / config.Config.MoonRad * float64(config.Config.ImageSize))
+	if (frac <= 1) && mp.Altitude >= 0 {
+		partFrac := (frac*float64(config.Config.ImageSize) - (float64(config.Config.ImageSize)/2 - config.Config.MoonRad)) / (2 * config.Config.MoonRad)
+		if partFrac >= 0 && partFrac <= 1 {
+			// two lines
+			y := math.Sqrt(1. - (partFrac-1./2.)*(partFrac-1./2.)*4.)
+			dc.DrawLine(config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize/2)-y*config.Config.MoonRad, float64(config.Config.ImageSize)*(1-frac))
+			dc.DrawLine(float64(config.Config.ImageSize)-config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize/2)+y*config.Config.MoonRad, float64(config.Config.ImageSize)*(1-frac))
+		} else {
+			// single line
+			dc.DrawLine(config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize)-config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac))
+		}
+	}
+	dc.Stroke()
+	return dc
+}
+
 func main() {
 	addHours := 0.
 	outPath := ""
 	noOut := false
 	ignoreCfg := false
+	runAsSrv := false
+	address := ":8778"
 	flag.Float64Var(&addHours, "forecast", 0, "add hours to time")
 	flag.StringVar(&outPath, "out", "out.png", "output path")
 	flag.BoolVar(&noOut, "noout", false, "do not output image")
@@ -45,6 +101,8 @@ func main() {
 	flag.BoolVar(&config.Config.HorizonLine, "horizonline", config.DefaultConfig.HorizonLine, "draw horizon line")
 	flag.Float64Var(&config.Config.HorizonLineWidth, "horizonlinewidth", config.DefaultConfig.HorizonLineWidth, "horizon line width")
 	flag.Float64Var(&config.Config.HorizonLinePadding, "horizonlinepadding", config.DefaultConfig.HorizonLinePadding, "horizon line padding")
+	flag.BoolVar(&runAsSrv, "server", false, "run as server")
+	flag.StringVar(&address, "hostname", ":8778", "server address")
 	flag.Parse()
 
 	/*you can do stuff like
@@ -56,59 +114,27 @@ func main() {
 	*/
 
 	config.Init()
+	if runAsSrv {
+		app := fiber.New()
+		app.Get("/*", func(c fiber.Ctx) error {
+			currTime := time.Now().Add(time.Duration(float64(time.Hour) * addHours))
+			mp := suncalc.GetMoonPosition(currTime, config.Config.Lat, config.Config.Lon)
+			mi := suncalc.GetMoonIllumination(currTime)
+			dc := genImage(mp, mi)
+			png.Encode(c.Response().BodyWriter(), dc.Image())
+			c.Type(".png")
+			return c.SendStatus(200)
+		})
+		log.Fatal(app.Listen(address))
+	} else {
+		currTime := time.Now().Add(time.Duration(float64(time.Hour) * addHours))
+		mp := suncalc.GetMoonPosition(currTime, config.Config.Lat, config.Config.Lon)
+		mi := suncalc.GetMoonIllumination(currTime)
 
-	currTime := time.Now().Add(time.Duration(float64(time.Hour) * addHours))
-	mp := suncalc.GetMoonPosition(currTime, config.Config.Lat, config.Config.Lon)
-	mi := suncalc.GetMoonIllumination(currTime)
-	visAngle := SignedAngleDiff(mi.Angle, mp.ParallacticAngle)
-	//fmt.Println(mp.ParallacticAngle, mi.Angle)
-	anglePhase := mi.Phase * (2 * math.Pi)
-	//fmt.Println(visAngle*360/(2*math.Pi), mi.Phase)
-	if !noOut {
-		dc := gg.NewContext(config.Config.ImageSize, config.Config.ImageSize)
-		dc.SetLineWidth(config.Config.LineWidth)
-		dc.DrawCircle(float64(config.Config.ImageSize)/2, float64(config.Config.ImageSize)/2, config.Config.MoonRad)
-		dc.SetColor(config.OutColor)
-		dc.Stroke()
-		currX, currY := screenCoords(1, 0, visAngle)
-		dc.MoveTo(currX, currY)
-		for i := 0; i < config.Config.LineRes; i++ {
-			phi := math.Pi * float64(i+1) / float64(config.Config.LineRes)
-			if math.Sin(anglePhase) > 0 {
-				currX, currY = screenCoords(math.Cos(phi), math.Sin(phi)*math.Cos(anglePhase), visAngle) // Should be opposite if sin < 0 for anglePhase
-			} else {
-				currX, currY = screenCoords(math.Cos(phi), -math.Sin(phi)*math.Cos(anglePhase), visAngle) // Should be opposite if sin < 0 for anglePhase
-			}
-			dc.LineTo(currX, currY)
+		if !noOut {
+			dc := genImage(mp, mi)
+			dc.SavePNG(outPath)
 		}
-		for i := 0; i < config.Config.LineRes; i++ {
-			phi := math.Pi * (1 - float64(i+1)/float64(config.Config.LineRes))
-			if math.Sin(anglePhase) > 0 {
-				currX, currY = screenCoords(math.Cos(phi), math.Sin(phi), visAngle) // Should be opposite if sin < 0 for anglePhase
-			} else {
-				currX, currY = screenCoords(math.Cos(phi), -math.Sin(phi), visAngle) // Should be opposite if sin < 0 for anglePhase
-			}
-			dc.LineTo(currX, currY)
-		}
-		dc.Fill()
-
-		dc.SetLineWidth(config.Config.HorizonLineWidth)
-		dc.SetDash(5, 10)
-		frac := mp.Altitude / (moonRadius / mp.Distance / config.Config.MoonRad * float64(config.Config.ImageSize))
-		if (frac <= 1) && mp.Altitude >= 0 {
-			partFrac := (frac*float64(config.Config.ImageSize) - (float64(config.Config.ImageSize)/2 - config.Config.MoonRad)) / (2 * config.Config.MoonRad)
-			if partFrac >= 0 && partFrac <= 1 {
-				// two lines
-				y := math.Sqrt(1. - (partFrac-1./2.)*(partFrac-1./2.)*4.)
-				dc.DrawLine(config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize/2)-y*config.Config.MoonRad, float64(config.Config.ImageSize)*(1-frac))
-				dc.DrawLine(float64(config.Config.ImageSize)-config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize/2)+y*config.Config.MoonRad, float64(config.Config.ImageSize)*(1-frac))
-			} else {
-				// single line
-				dc.DrawLine(config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac), float64(config.Config.ImageSize)-config.Config.HorizonLinePadding, float64(config.Config.ImageSize)*(1-frac))
-			}
-		}
-		dc.Stroke()
-		dc.SavePNG(outPath)
+		fmt.Printf("∠ %0.2f°, θ %0.2f°", 180./math.Pi*mp.Altitude, 180+180./math.Pi*mp.Azimuth)
 	}
-	fmt.Printf("∠ %0.2f°, θ %0.2f°", 180./math.Pi*mp.Altitude, 180+180./math.Pi*mp.Azimuth)
 }
